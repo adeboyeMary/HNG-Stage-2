f0a6de23121364ed42c229536b7866ee0200df58
Adeboye Mary <maryadeboye69@gmail.com>
2024-07-10 11:33:38 +0100

    ...

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,24 +8,25 @@ import icon2 from '../asset/svg/icon2.svg';
 
 const Footer = () => {
     return (
-        <div>
+        <div >
             <div></div>
             <footer>
-                <div className="flex flex-row">
-                    <span><img src={ig} alt="ig" /></span>
-                    <span><img src={x} alt="x" /></span>
-                    <span><img src={icon} alt="icon" /></span>
-                    <span><img src={icon5} alt="icon5" /></span>
-                    <span><img src={icon2} alt="icon2" /></span>
+                <div className="flex flex-row justify-center my-10">
+                    <span><img src={ig} alt="ig" className='ml-[20px]' /></span>
+                    <span><img src={x} alt="x" className='mt-[4px] ml-[20px]' /></span>
+                    <span><img src={icon} alt="icon" className='ml-[20px]' /></span>
+                    <span><img src={icon5} alt="icon5" className='ml-[20px]' /></span>
+                    <span><img src={icon2} alt="icon2" className='ml-[20px]' /></span>
                 </div>
 
-                <div className="flex flex-row">
-                    <span>Policy</span>
-                    <span>FAQs</span>
-                    <span>Term of use</span>
+                <div className="flex flex-row justify-center mt-8 mb-5">
+                    <span className='ml-16 hover:text-[#E52659] hover:font-bold'>Privacy Policy</span>
+                    <span className='ml-16 hover:text-[#E52659] hover:font-bold'>FAQs</span>
+                    <span className='ml-16 hover:text-[#E52659] hover:font-bold'>Term of use</span>
                 </div>
 
-                <span>Sonjinwoostores&copy; 2020 - 2023, All Rghts Reserved</span>
+                <div  className='text-center mb-5 ml-[4rem]'>
+                    <span>Sonjinwoostores&copy; 2020 - 2023, All Rghts Reserved</span></div>
             </footer>
         </div>
     )
